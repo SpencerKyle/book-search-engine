69b5e9432a8113d509418e9e734334377b787462
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-09 17:44:04 -0700

    changed app.js and added mutations - queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+const gql = require('graphql-tag');
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
